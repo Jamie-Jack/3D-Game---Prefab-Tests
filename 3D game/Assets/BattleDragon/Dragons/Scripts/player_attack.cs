cf33a00a248a82e80dcf2c72379281639360dc7f
Jamie-Jack <jgidley@uclan.ac.uk>
2019-11-12 14:23:28 +0000

    3d game
    3d

---
+++ 3D game/Assets/BattleDragon/Dragons/Scripts/player_attack.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_attack : MonoBehaviour
+{
+    private Animator anim; 
+
+    // Use this for initialization
+    void Awake()
+    {
+        // Set up references.
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        // Attack
+        if (Input.GetButtonDown("J"))
+        {
+            anim.SetTrigger("Attack_1");
+            return;
+        }
+
+        // dead
+        if (Input.GetButtonDown("1"))
+        {
+            anim.SetTrigger("Dead");
+            return;
+        }
+
+        // damage
+        if (Input.GetButtonDown("2"))
+        {
+            anim.SetTrigger("Damage");
+            return;
+        }
+
+        // burst
+        if (Input.GetButtonDown("3"))
+        {
+            anim.SetTrigger("Burst");
+            return;
+        }
+
+        // defense
+        if (Input.GetButtonDown("4"))
+        {
+            anim.SetTrigger("Defense");
+            return;
+        }
+    }
+}
